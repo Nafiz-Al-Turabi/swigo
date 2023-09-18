837ed646eab7172357188de12b5676ce557fc618
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2023-09-18 23:07:13 +0600

    Scroll to top Button added

---
+++ src/Components/ScrollToTop/ScrollToTopButton.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { AiOutlineArrowUp } from "react-icons/ai";
+import './ScrollToTopButton.css'
+
+function ScrollToTopButton() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        isVisible && (
+            <button
+                onClick={scrollToTop}
+                className="scroll-to-top-button">
+                <AiOutlineArrowUp className='text-3xl' />
+            </button>
+        )
+    );
+}
+
+export default ScrollToTopButton;
