5945e6ea7ce893c7b7e9538cf526df70abf9ca92
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2023-09-20 23:17:17 +0600

    Loading Screen added

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -3,8 +3,13 @@ import './Loading.css'
 
 const Loading = () => {
     return (
-        <div className="loading-overlay">
-             <div style={{fontFamily:'Lobster'}} className="loading-text">SWIGO</div>
+        <div className="loading-overlay flex items-center">
+            <img className='h-32 mr-10' src="https://i.ibb.co/hB44rNz/line.gif" alt="" />
+
+            <div style={{ fontFamily: 'Lobster' }} className="loading-text ">
+                SWIGO
+            </div>
+            <img className='h-32' src="https://i.ibb.co/4ZJLt0Y/good-food.gif" alt="" />
         </div>
     );
 };
